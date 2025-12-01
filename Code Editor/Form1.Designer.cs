407006f35b7ec23ffd69d916d708d35148314c48
mudasarmajeed5 <mudasarmajeed5@yahoo.com>
2025-12-01 23:35:57 +0500

    Initial commit with local files

---
+++ Code Editor/Form1.Designer.cs
@@ -0,0 +1,247 @@
+﻿namespace Code_Editor
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            splitContainer1 = new SplitContainer();
+            splitContainer2 = new SplitContainer();
+            editor = new Microsoft.Web.WebView2.WinForms.WebView2();
+            code_output = new RichTextBox();
+            fileToolStripMenuItem = new ToolStripMenuItem();
+            openToolStripMenuItem = new ToolStripMenuItem();
+            saveToolStripMenuItem = new ToolStripMenuItem();
+            exitToolStripMenuItem = new ToolStripMenuItem();
+            editToolStripMenuItem = new ToolStripMenuItem();
+            helpToolStripMenuItem = new ToolStripMenuItem();
+            followToolStripMenuItem = new ToolStripMenuItem();
+            run_code = new ToolStripMenuItem();
+            crownMenuStrip1 = new ReaLTaiizor.Controls.CrownMenuStrip();
+            ((System.ComponentModel.ISupportInitialize)splitContainer1).BeginInit();
+            splitContainer1.Panel2.SuspendLayout();
+            splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)splitContainer2).BeginInit();
+            splitContainer2.Panel1.SuspendLayout();
+            splitContainer2.Panel2.SuspendLayout();
+            splitContainer2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)editor).BeginInit();
+            crownMenuStrip1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            splitContainer1.Dock = DockStyle.Fill;
+            splitContainer1.Location = new Point(3, 76);
+            splitContainer1.Margin = new Padding(3, 2, 3, 2);
+            splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            splitContainer1.Panel1.BackColor = Color.FromArgb(32, 32, 32);
+            // 
+            // splitContainer1.Panel2
+            // 
+            splitContainer1.Panel2.Controls.Add(splitContainer2);
+            splitContainer1.Size = new Size(1125, 470);
+            splitContainer1.SplitterDistance = 192;
+            splitContainer1.TabIndex = 0;
+            // 
+            // splitContainer2
+            // 
+            splitContainer2.BackColor = Color.FromArgb(64, 64, 64);
+            splitContainer2.Dock = DockStyle.Fill;
+            splitContainer2.Location = new Point(0, 0);
+            splitContainer2.Margin = new Padding(3, 2, 3, 2);
+            splitContainer2.Name = "splitContainer2";
+            // 
+            // splitContainer2.Panel1
+            // 
+            splitContainer2.Panel1.Controls.Add(editor);
+            // 
+            // splitContainer2.Panel2
+            // 
+            splitContainer2.Panel2.BackColor = Color.FromArgb(32, 32, 32);
+            splitContainer2.Panel2.Controls.Add(code_output);
+            splitContainer2.Size = new Size(929, 470);
+            splitContainer2.SplitterDistance = 607;
+            splitContainer2.TabIndex = 0;
+            // 
+            // editor
+            // 
+            editor.AllowExternalDrop = true;
+            editor.BackColor = SystemColors.Control;
+            editor.CreationProperties = null;
+            editor.DefaultBackgroundColor = Color.White;
+            editor.Dock = DockStyle.Fill;
+            editor.Location = new Point(0, 0);
+            editor.Margin = new Padding(0);
+            editor.Name = "editor";
+            editor.Size = new Size(607, 470);
+            editor.TabIndex = 0;
+            editor.ZoomFactor = 1D;
+            // 
+            // code_output
+            // 
+            code_output.BackColor = Color.FromArgb(32, 32, 32);
+            code_output.Dock = DockStyle.Fill;
+            code_output.ForeColor = SystemColors.Info;
+            code_output.Location = new Point(0, 0);
+            code_output.Margin = new Padding(3, 2, 3, 2);
+            code_output.Name = "code_output";
+            code_output.Size = new Size(318, 470);
+            code_output.TabIndex = 0;
+            code_output.Text = "";
+            // 
+            // fileToolStripMenuItem
+            // 
+            fileToolStripMenuItem.BackColor = Color.FromArgb(60, 63, 65);
+            fileToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { openToolStripMenuItem, saveToolStripMenuItem, exitToolStripMenuItem });
+            fileToolStripMenuItem.ForeColor = Color.FromArgb(220, 220, 220);
+            fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            fileToolStripMenuItem.Size = new Size(46, 24);
+            fileToolStripMenuItem.Text = "File";
+            // 
+            // openToolStripMenuItem
+            // 
+            openToolStripMenuItem.BackColor = Color.FromArgb(60, 63, 65);
+            openToolStripMenuItem.ForeColor = Color.FromArgb(220, 220, 220);
+            openToolStripMenuItem.Name = "openToolStripMenuItem";
+            openToolStripMenuItem.ShortcutKeys = Keys.Control | Keys.O;
+            openToolStripMenuItem.Size = new Size(181, 26);
+            openToolStripMenuItem.Text = "Open";
+            openToolStripMenuItem.Click += openToolStripMenuItem_Click;
+            // 
+            // saveToolStripMenuItem
+            // 
+            saveToolStripMenuItem.BackColor = Color.FromArgb(60, 63, 65);
+            saveToolStripMenuItem.ForeColor = Color.FromArgb(220, 220, 220);
+            saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            saveToolStripMenuItem.ShortcutKeys = Keys.Control | Keys.S;
+            saveToolStripMenuItem.Size = new Size(181, 26);
+            saveToolStripMenuItem.Text = "Save";
+            // 
+            // exitToolStripMenuItem
+            // 
+            exitToolStripMenuItem.BackColor = Color.FromArgb(60, 63, 65);
+            exitToolStripMenuItem.ForeColor = Color.FromArgb(220, 220, 220);
+            exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            exitToolStripMenuItem.Size = new Size(181, 26);
+            exitToolStripMenuItem.Text = "Exit";
+            // 
+            // editToolStripMenuItem
+            // 
+            editToolStripMenuItem.BackColor = Color.FromArgb(60, 63, 65);
+            editToolStripMenuItem.ForeColor = Color.FromArgb(220, 220, 220);
+            editToolStripMenuItem.Name = "editToolStripMenuItem";
+            editToolStripMenuItem.Size = new Size(49, 24);
+            editToolStripMenuItem.Text = "Edit";
+            // 
+            // helpToolStripMenuItem
+            // 
+            helpToolStripMenuItem.BackColor = Color.FromArgb(60, 63, 65);
+            helpToolStripMenuItem.ForeColor = Color.FromArgb(220, 220, 220);
+            helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            helpToolStripMenuItem.Size = new Size(55, 24);
+            helpToolStripMenuItem.Text = "Help";
+            // 
+            // followToolStripMenuItem
+            // 
+            followToolStripMenuItem.BackColor = Color.FromArgb(60, 63, 65);
+            followToolStripMenuItem.ForeColor = Color.FromArgb(220, 220, 220);
+            followToolStripMenuItem.Name = "followToolStripMenuItem";
+            followToolStripMenuItem.Size = new Size(67, 24);
+            followToolStripMenuItem.Text = "Follow";
+            // 
+            // run_code
+            // 
+            run_code.BackColor = Color.FromArgb(60, 63, 65);
+            run_code.ForeColor = Color.FromArgb(220, 220, 220);
+            run_code.Name = "run_code";
+            run_code.ShortcutKeys = Keys.Control | Keys.F5;
+            run_code.Size = new Size(48, 24);
+            run_code.Text = "Run";
+            run_code.Click += run_code_Click;
+            // 
+            // crownMenuStrip1
+            // 
+            crownMenuStrip1.BackColor = Color.Black;
+            crownMenuStrip1.ForeColor = Color.FromArgb(220, 220, 220);
+            crownMenuStrip1.ImageScalingSize = new Size(20, 20);
+            crownMenuStrip1.Items.AddRange(new ToolStripItem[] { fileToolStripMenuItem, editToolStripMenuItem, helpToolStripMenuItem, followToolStripMenuItem, run_code });
+            crownMenuStrip1.Location = new Point(3, 48);
+            crownMenuStrip1.Name = "crownMenuStrip1";
+            crownMenuStrip1.Padding = new Padding(3, 2, 0, 2);
+            crownMenuStrip1.Size = new Size(1125, 28);
+            crownMenuStrip1.TabIndex = 1;
+            crownMenuStrip1.Text = "crownMenuStrip1";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.Gray;
+            ClientSize = new Size(1131, 548);
+            Controls.Add(splitContainer1);
+            Controls.Add(crownMenuStrip1);
+            Font = new Font("Segoe UI", 9F);
+            MainMenuStrip = crownMenuStrip1;
+            Margin = new Padding(3, 2, 3, 2);
+            Name = "Form1";
+            Padding = new Padding(3, 48, 3, 2);
+            Text = "BARI CHURAIIIL";
+            Load += Form1_Load;
+            splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)splitContainer1).EndInit();
+            splitContainer1.ResumeLayout(false);
+            splitContainer2.Panel1.ResumeLayout(false);
+            splitContainer2.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)splitContainer2).EndInit();
+            splitContainer2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)editor).EndInit();
+            crownMenuStrip1.ResumeLayout(false);
+            crownMenuStrip1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private SplitContainer splitContainer1;
+        private SplitContainer splitContainer2;
+        private Microsoft.Web.WebView2.WinForms.WebView2 editor;
+        private RichTextBox code_output;
+        private ToolStripMenuItem fileToolStripMenuItem;
+        private ToolStripMenuItem openToolStripMenuItem;
+        private ToolStripMenuItem saveToolStripMenuItem;
+        private ToolStripMenuItem exitToolStripMenuItem;
+        private ToolStripMenuItem editToolStripMenuItem;
+        private ToolStripMenuItem helpToolStripMenuItem;
+        private ToolStripMenuItem followToolStripMenuItem;
+        private ToolStripMenuItem run_code;
+        private ReaLTaiizor.Controls.CrownMenuStrip crownMenuStrip1;
+    }
+}
---
+++ Code Editor/Form1.cs
@@ -0,0 +1,194 @@
+using Microsoft.Web.WebView2.WinForms;
+using ReaLTaiizor.Forms;
+using System.Windows.Forms;
+using System.Text.Json;
+using System.Diagnostics;
+
+namespace Code_Editor
+{
+    public partial class Form1 : ReaLTaiizor.Forms.LostForm
+
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeWebView();
+        }
+        
+        private Dictionary<string, string> openFiles = new Dictionary<string, string>();
+        private Dictionary<string, Button> fileButtons = new Dictionary<string, Button>();
+        private string currentFile = "";
+        private Button activeButton = null;
+        private void OpenFile()
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                string fileName = Path.GetFileName(ofd.FileName);
+                string content = File.ReadAllText(ofd.FileName);
+
+                if (!openFiles.ContainsKey(fileName))
+                {
+                    openFiles[fileName] = content;
+
+                    Button fileBtn = new Button();
+                    fileBtn.Text = fileName;
+                    fileBtn.Dock = DockStyle.Top;
+                    fileBtn.Padding = new Padding(10, 5, 10, 5);
+                    fileBtn.Height = 35;
+                    fileBtn.FlatStyle = FlatStyle.Flat;
+                    fileBtn.FlatAppearance.BorderSize = 0;
+                    fileBtn.BackColor = ColorTranslator.FromHtml("#2d2d30");
+                    fileBtn.ForeColor = Color.White;
+                    fileBtn.TextAlign = ContentAlignment.MiddleLeft;
+                    fileBtn.Click += (s, e) => SwitchToFile(fileName);
+                    
+                    fileButtons[fileName] = fileBtn;
+                    splitContainer1.Panel1.Controls.Add(fileBtn);
+                }
+
+                SwitchToFile(fileName);
+            }
+        }
+
+        private async void SwitchToFile(string fileName)
+        {
+            // Reset previous active button
+            if (activeButton != null)
+            {
+                activeButton.BackColor = ColorTranslator.FromHtml("#2d2d30");
+                activeButton.ForeColor = Color.White;
+            }
+
+            // Set new active button
+            if (fileButtons.ContainsKey(fileName))
+            {
+                activeButton = fileButtons[fileName];
+                activeButton.BackColor = ColorTranslator.FromHtml("#007ACC"); // VS Code blue
+                activeButton.ForeColor = Color.White;
+            }
+
+            currentFile = fileName;
+            
+            string code = openFiles[fileName];
+            string escapedCode = System.Text.Json.JsonSerializer.Serialize(code);
+            await editor.ExecuteScriptAsync($"setEditorCode({escapedCode});");
+        }
+
+
+
+        private async void InitializeWebView()
+        {
+            await editor.EnsureCoreWebView2Async();
+            string htmlPath = Path.Combine(Application.StartupPath, "index.html");
+            editor.Source = new Uri(htmlPath);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            splitContainer1.BackColor = ColorTranslator.FromHtml("#202020");
+            splitContainer2.BackColor = ColorTranslator.FromHtml("#202020");
+        }
+
+        private async void run_code_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string fileExtension = Path.GetExtension(currentFile).ToLower();
+
+                if (fileExtension == ".cpp" || fileExtension == ".cc" || fileExtension == ".cxx" || fileExtension == ".c")
+                {
+                    string cppCode = openFiles[currentFile];
+                    string output = await ExecuteCppCode(cppCode);
+                    code_output.Text = output;
+                }
+                else
+                {
+                    string outputJson = await editor.ExecuteScriptAsync("executeCode();");
+                    
+                    string output = System.Text.Json.JsonSerializer.Deserialize<string>(outputJson);
+                    code_output.Text = output;
+                }
+            }
+            catch (Exception ex)
+            {
+                code_output.Text = "Exception: " + ex.Message;
+            }
+        }
+
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFile();
+        }
+
+        // C++ Execution Methods
+        private async Task<string> ExecuteCppCode(string cppCode)
+        {
+            string tempDir = Path.Combine(Path.GetTempPath(), "CppCodeRunner");
+            Directory.CreateDirectory(tempDir);
+
+            string sourceFile = Path.Combine(tempDir, "temp.cpp");
+            string exeFile = Path.Combine(tempDir, "temp.exe");
+
+            try
+            {
+                // Write C++ code to temporary file
+                File.WriteAllText(sourceFile, cppCode);
+
+                // Compile with GCC
+                string compileOutput = RunCommand("gcc", $"\"{sourceFile}\" -o \"{exeFile}\"");
+                if (!File.Exists(exeFile))
+                {
+                    return "Compilation Error:\n" + compileOutput;
+                }
+
+                // Execute the compiled program
+                string executionOutput = RunCommand(exeFile, "");
+                return executionOutput;
+            }
+            catch (Exception ex)
+            {
+                return "Error: " + ex.Message;
+            }
+            finally
+            {
+                // Cleanup
+                try
+                {
+                    if (File.Exists(sourceFile)) File.Delete(sourceFile);
+                    if (File.Exists(exeFile)) File.Delete(exeFile);
+                }
+                catch { }
+            }
+        }
+
+        private string RunCommand(string command, string arguments)
+        {
+            try
+            {
+                ProcessStartInfo psi = new ProcessStartInfo
+                {
+                    FileName = command,
+                    Arguments = arguments,
+                    UseShellExecute = false,
+                    RedirectStandardOutput = true,
+                    RedirectStandardError = true,
+                    CreateNoWindow = true
+                };
+
+                using (Process process = Process.Start(psi))
+                {
+                    string output = process.StandardOutput.ReadToEnd();
+                    string error = process.StandardError.ReadToEnd();
+                    process.WaitForExit();
+                    return string.IsNullOrEmpty("error") ? output : error;
+                }
+            }
+            catch (Exception ex)
+            {
+                return "Failed to execute: " + ex.Message;
+            }
+        }
+    }
+}
